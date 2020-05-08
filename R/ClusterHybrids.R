c36595d6946ab96b4168171c22253bf01e79bd71
Anob Chakrabarti <anob.chakrabarti@crick.ac.uk>
2020-05-08 12:59:34 +0100

    Added finding overlaps and clustering functions

--- R/ClusterHybrids.R
+++ R/ClusterHybrids.R
@@ -1,77 +1,92 @@
-#
-#
-# ClusterHybrids <- function(hybrids.dt) {
-#
-#   L.gr <- ConvertToGRanges(hybrids.dt, arm = "L")
-#   R.gr <- ConvertToGRanges(hybrids.dt, arm = "R")
-#
-#   setorder(hybrids.dt, L_seqnames)
-#   L.hybrids.dt <- split(hybrids.dt, by = "L_seqnames")
-#
-#   # Find self hits
-#   L.gr <- ConvertToGRanges(L.hybrids.dt$Mapt_ENSMUSG00000018411.17, arm = "L")
-#
-#   L.ol <- GenomicRanges::findOverlaps(L.gr, drop.self = TRUE, drop.redundant = TRUE)
-#
-#   # Keep 75% overlap
-#   x <- L.gr[queryHits(L.ol)]
-#   y <- L.gr[subjectHits(L.ol)]
-#   relative_overlap <- width(pintersect(x, y)) / pmin(width(x), width(y))
-#   L.ol <- L.ol[relative_overlap >= 0.75]
-#
-#   clusters <- extractClustersFromSelfHits(L.ol)
-#
-# }
-#
-# # https://support.bioconductor.org/p/85092/
-# extractClustersFromSelfHits <- function(hits)
-# {
-#   stopifnot(is(hits, "Hits"))
-#   N <- queryLength(hits)
-#   stopifnot(N == subjectLength(hits))
-#   h <- union(hits, t(hits))
-#   qh <- queryHits(h)
-#   sh <- subjectHits(h)
-#   cid <- cid0 <- seq_len(N)  # cluster ids
-#   while (TRUE) {
-#     cid2 <- pmin(cid, selectHits(h, "first"))
-#     if (identical(cid2, cid))
-#       break
-#     cid <- cid2
-#     h <- Hits(qh, cid[sh], N, N)
-#   }
-#   unname(splitAsList(cid0, cid))
-# }
-#
-# x <- c("a", "b", "a", "c", "d")
-# table <- c("a", "e", "d", "a", "a", "d")
-# hits <- findMatches(x, table)  # sorts the hits by query
-# hits
-#
-# selectHits(hits, select="all")  # no-op
-#
-# selectHits(hits, select="first")
-# selectHits(hits, select="first", nodup=TRUE)
-#
-# selectHits(hits, select="last")
-# selectHits(hits, select="last", nodup=TRUE)
-#
-# selectHits(hits, select="arbitrary")
-# selectHits(hits, select="count")
-#
-# # ========
-#
-# L.ol <- GenomicRanges::findOverlaps(L.gr, drop.self = FALSE, drop.redundant = FALSE)
-# NEL <- split(subjectHits(L.ol), queryHits(L.ol))
-# test <- graph::graphNEL(as.character(seq_len(queryLength(L.ol))), NEL)
-#
-# comp <- RBGL::connectedComp(test)
-# comp.dt <- data.table(cluster = rep(names(comp), elementNROWS(comp)),
-#                       id = as.integer(unlist(comp)))
-#
-# L.gr$id <- 1:length(L.gr)
-# L.gr$cluster <- comp.dt$cluster[match(L.gr$id, comp.dt$id)]
-#
-# comp.dt <- as.data.table(comp)
-#
-# test@partitioning
+
+
+
+
+#' FindHybridOverlaps
+#'
+#' @param hybrids.x.dt
+#' @param hybrids.y.dt
+#' @param percent_overlap
+#' @param reciprocal
+#'
+#' @return
+#'
+#' @import data.table
+#' @export
+
+FindHybridOverlaps <- function(hybrids.x.dt, hybrids.y.dt, percent_overlap = 0.5, reciprocal = FALSE) {
+
+  hybrids.x.dt[, `:=` (m = 1, idx = 1:.N)]
+  hybrids.y.dt[, `:=` (m = 1, idx = 1:.N)]
+
+  cj.hybrids.dt <- merge(hybrids.x.dt, hybrids.y.dt, by = "m", allow.cartesian = TRUE)
+
+  if(reciprocal == FALSE) cj.hybrids.dt <- cj.hybrids.dt[name.x < name.y] # Don't do this for edges
+  cj.hybrids.dt <- cj.hybrids.dt[name.x != name.y]
+
+  # Calculate overlaps
+  cj.hybrids.dt[, `:=` (L_ol = min(L_end.x, L_end.y) - max(L_start.x, L_start.y) + 1,
+                        R_ol = min(R_end.x, R_end.y) - max(R_start.x, R_start.y) + 1),
+               by = .(name.x, name.y)]
+  cj.hybrids.dt <- cj.hybrids.dt[L_ol > 0 & R_ol > 0]
+
+  # If no overlaps, return empty data.table
+  if(nrow(cj.hybrids.dt) == 0) return(cj.hybrids.dt[, `:=` (L_p = NA, R_p = NA)][, .(idx.x, name.x, idx.y, name.y, L_p, R_p)])
+
+  # Calculate span
+  cj.hybrids.dt[, `:=` (L_sp = max(L_end.x, L_end.y) - min(L_start.x, L_start.y) + 1,
+                        R_sp = max(R_end.x, R_end.y) - min(R_start.x, R_start.y) + 1),
+                by = .(name.x, name.y)]
+
+  # Calculate percentage overlaps
+  cj.hybrids.dt[, `:=` (L_p = L_ol/L_sp,
+                        R_p = R_ol/R_sp),
+                by = .(name.x, name.y)]
+
+  # Filter based on percentage overlap
+  cj.hybrids.dt <- cj.hybrids.dt[L_p > percent_overlap & R_p > percent_overlap]
+
+  hits.dt <- cj.hybrids.dt[, .(idx.x, name.x, idx.y, name.y, L_p, R_p)]
+  return(hits.dt)
+
+}
+
+#' Title
+#'
+#' @param hybrids.dt
+#' @param percent_overlap
+#'
+#' @return
+#'
+#' @import data.table
+#' @export
+
+ClusterHybrids <- function(hybrids.dt, percent_overlap = 0.5) {
+
+  # Get overlap hits
+  hits.dt <- FindHybridOverlaps(hybrids.x.dt = hybrids.dt, hybrids.y.dt = hybrids.dt, percent_overlap = percent_overlap, reciprocal = TRUE)
+
+  # If not hits, return all clusters as NA
+  if(nrow(hits.dt) == 0) return(hybrids.dt[, cluster := NA])
+
+  # Convert to graph
+  n <- as.character(unique(hits.dt$name.x))
+  e <- split(as.character(hits.dt$name.y), as.character(hits.dt$name.x))
+  NEL <- graph::graphNEL(nodes = n, edgeL = e, edgemode = "undirected")
+
+  # Get connected nodes (i.e. clusters)
+  clusters <- RBGL::connectedComp(NEL) # https://support.bioconductor.org/p/85092/
+  # table(elementNROWS(clusters))
+  clusters.dt <- data.table(cluster = rep(names(clusters), elementNROWS(clusters)),
+                            name = unlist(clusters))
+  setkey(clusters.dt, name)
+
+  stopifnot(any(!duplicated(clusters.dt$name))) # Make sure no hybrid is in more than one cluster
+
+  # Merge back
+  setkey(hybrids.dt, name)
+  hybrids.dt <- merge(hybrids.dt, clusters.dt, by = "name", all.x = TRUE)
+
+  return(hybrids.dt)
+
+}
