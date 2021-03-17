edf659be75eeceef71203bb2debc3e1273ff32ac
Ira Iosub <ira.iosub@gmail.com>
2021-03-17 20:15:55 +0000

    updated slurm functions to export db

--- R/slurm.R
+++ R/slurm.R
@@ -11,13 +11,13 @@
 
   binding <- GetMFE(L_sequence, R_sequence)
   mfe <- binding$mfe
-  structure <- binding$structure
+  db <- binding$db
   names(mfe) <- id
-  names(structure) <- id
+  names(db) <- id
   
-  #return(list(id = id, mfe = mfe, structure = structure))
+  #return(list(id = id, mfe = mfe, db = db))
   
-  mfe.dt <- as.data.table(list(id = id, mfe = mfe, structure = structure))
+  mfe.dt <- as.data.table(list(id = id, mfe = mfe, db = db))
   return(mfe.dt)  # returning as data.table could be more convenient? after rslurm can rbindlist
 
 }
@@ -35,25 +35,25 @@
   # Get MFE
   binding <- GetMFE(L_sequence, R_sequence)
   mfe <- binding$mfe
-  structure <- binding$structure
+  db <- binding$db
   
   L <- sapply(seq_along(id), function(i) ShuffleSequence(L_sequence[i], number = 10, klet = 2))
   R <- sapply(seq_along(id), function(i) ShuffleSequence(R_sequence[i], number = 10, klet = 2))
   
-  shuffledmfe <- sapply(1:ncol(L),function(i) GetMFE(L[,i], R[,i])$mfe)  #only care about mfe for shuffled
+  shuffledmfe <- sapply(1:ncol(L),function(i) GetMFE(L[,i], R[,i])$mfe)  #only need mfe for shuffled
   
-  results.ls <- list(id = id, mfe = mfe, structure = structure,
+  results.ls <- list(id = id, mfe = mfe, db = db,
                      shuffled_mean = colMeans(shuffledmfe),
                      shuffled_sd = apply(shuffledmfe, 2, sd))
   
-  # return(list(id = id, mfe = mfe, structure = structure,
+  # return(list(id = id, mfe = mfe, db = db,
   #             shuffled_mean = colMeans(shuffledmfe),
   #             shuffled_sd = apply(shuffledmfe, 2, sd)))
   mfe.dt <- as.data.table(results.ls)
   return(mfe.dt) # returning as data.table could be more convenient? after rslurm can rbindlist
   
 
-  # # Get MFE
+  # # Get MFE 
   # mfe <- GetMFE(L_sequence, R_sequence)
   # 
   # L <- ShuffleSequence(L_sequence, number = 100, klet = 2)
