c53e9ab714536ee232a327b7936fa8c49d3bb67e
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-04-11 18:47:43 +0100

    Updated RNAduplex functions

--- R/get_structure.R
+++ R/get_structure.R
@@ -1,4 +1,52 @@
 
+#' Title
+#'
+#' @param hybrids.dt
+#' @param genome.dt
+#'
+#' @return
+#'
+#' @import data.table
+#' @export
+
+get_sequence <- function(hybrids.dt, genome.dt) {
+
+  # Get L
+  hybrids.dt[, gene_id := L_seqnames]
+  setkey(genome.dt, gene_id)
+  setkey(hybrids.dt, gene_id)
+  seq.dt <- genome.dt[hybrids.dt]
+  seq.dt[, `:=`(L_sequence, stringr::str_sub(sequence, start = L_start, end = L_end))]
+  seq.dt[, `:=` (c("gene_id", "sequence"), NULL)]
+
+  # Get R
+  # Do separately in case on different genes
+  seq.dt[, gene_id := R_seqnames]
+  setkey(seq.dt, gene_id)
+  seq.dt <- genome.dt[seq.dt]
+  seq.dt[, `:=`(R_sequence, stringr::str_sub(sequence, start = R_start, end = R_end))]
+  seq.dt[, `:=` (c("gene_id", "sequence"), NULL)]
+
+  return(seq.dt)
+
+}
+
+
+
+#' Title
+#'
+#' @param sequence
+#' @param klet
+#' @param seed
+#'
+#' @return
+#' @export
+
+shuffle_sequence <- function(sequence, number = 1, klet = 2, seed = 42) {
+
+  system(paste0("uShuffle -seed ", seed, " -k ", klet, " -n ", number, " -s ", sequence), intern = TRUE)
+
+}
 
 #' Title
 #'
@@ -12,7 +60,7 @@
 #'
 #' @examples
 
-get_structure <- function(L_sequence, R_sequence) {
+analyse_structure <- function(L_sequence, R_sequence) {
 
   input <- paste0(L_sequence, "\n", R_sequence)
   rnaduplex <- system("RNAduplex --noLP", input = input, intern = TRUE)
