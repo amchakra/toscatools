fe1c12a72384c0f5768131395a22621430f2d3a1
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-04-14 22:24:00 +0100

    Added documentation for cluster_hybrids and contact_maps

--- R/cluster_hybrids.R
+++ R/cluster_hybrids.R
@@ -1,83 +1,93 @@
-#' Title
+# ==========
+# Functions to cluster hybrids
+# ==========
+
+#' Find overlapping pairs of hybrids
 #'
-#' @param hybrids.dt
-#' @param percent_overlap
+#' Uses \code{bedtools pairtopair} to get all overlaps and then calculates percentage overlaps based on overall span of overlapping L and R arms.
+#' Called by \code{cluster_hybrids}
 #'
-#' @return
-#'
+#' @param hybrids.dt Hybrids data.table
+#' @param verbose Print \code{bedtools} command
+#' @return bedpe.dt with L, R and mean percentage overlap calculated
 #' @import data.table
 #' @export
 
 find_hybrid_overlaps <- function(hybrids.dt, verbose = FALSE) {
-
   hybrids.dt <- primavera::reorient_hybrids(hybrids.dt)
 
   # Create BEDPE and get overlaps
   bedpe.colnames <- c("L_seqnames", "L_start", "L_end", "R_seqnames", "R_start", "R_end", "name", "total_count", "L_strand", "R_strand")
   bedpe.dt <- hybrids.dt[, ..bedpe.colnames]
-  bedpe.dt[, `:=` (L_start = L_start - 1, R_start = R_start - 1)]
+  bedpe.dt[, `:=`(
+    L_start = L_start - 1,
+    R_start = R_start - 1
+  )]
 
   bedpe <- tempfile(tmpdir = getwd(), fileext = ".bedpe")
   ol <- tempfile(tmpdir = getwd(), fileext = ".bedpe")
 
   fwrite(bedpe.dt, file = bedpe, sep = "\t", col.names = FALSE)
   cmd <- paste("bedtools pairtopair -rdn -a", bedpe, "-b", bedpe, ">", ol)
-  if(verbose) message(cmd)
+  if (verbose) message(cmd)
   system(cmd)
 
   # Check if there are no overlaps
-  if(file.size(ol) != 0) {
-
+  if (file.size(ol) != 0) {
     bedpe.dt <- fread(ol, col.names = c(paste0(bedpe.colnames, ".x"), paste0(bedpe.colnames, ".y")))
     # Delete temporary files
     invisible(file.remove(bedpe))
     invisible(file.remove(ol))
-
   } else {
 
     # Delete temporary files
     invisible(file.remove(bedpe))
     invisible(file.remove(ol))
     return(data.table())
-
   }
 
   # Get calculations and filter
-  bedpe.dt[, `:=` (L_ol = min(L_end.x, L_end.y) - max(L_start.x, L_start.y) + 1,
-                   R_ol = min(R_end.x, R_end.y) - max(R_start.x, R_start.y) + 1),
-           by = .(name.x, name.y)]
+  bedpe.dt[, `:=`(
+    L_ol = min(L_end.x, L_end.y) - max(L_start.x, L_start.y) + 1,
+    R_ol = min(R_end.x, R_end.y) - max(R_start.x, R_start.y) + 1
+  ),
+  by = .(name.x, name.y)
+  ]
 
-  bedpe.dt[, `:=` (L_sp = max(L_end.x, L_end.y) - min(L_start.x, L_start.y) + 1,
-                   R_sp = max(R_end.x, R_end.y) - min(R_start.x, R_start.y) + 1),
-           by = .(name.x, name.y)]
+  bedpe.dt[, `:=`(
+    L_sp = max(L_end.x, L_end.y) - min(L_start.x, L_start.y) + 1,
+    R_sp = max(R_end.x, R_end.y) - min(R_start.x, R_start.y) + 1
+  ),
+  by = .(name.x, name.y)
+  ]
 
-  bedpe.dt[, `:=` (L_p = L_ol/L_sp,
-                   R_p = R_ol/R_sp),
-           by = .(name.x, name.y)]
+  bedpe.dt[, `:=`(
+    L_p = L_ol / L_sp,
+    R_p = R_ol / R_sp
+  ),
+  by = .(name.x, name.y)
+  ]
 
   bedpe.dt[, mean_p := mean(c(L_p, R_p)), by = .(name.x, name.y)]
   return(bedpe.dt)
-
 }
 
-#' Title
+#' Cluster overlapping hybrids
 #'
-#' @param hybrid.dt
-#' @param sample_size
-#' @param percent_overlap
-#' @param cores
+#' Cluster overlapping hybrids using graph network analysis
 #'
+#' @param hybrid.dt Hybrid data.table
+#' @param percent_overlap Minimum percentage overlap for both L and R arms
 #' @return
 #' @import data.table
 #' @export
-#'
-#' @examples
 
 cluster_hybrids <- function(hybrids.dt, percent_overlap = 0.75, verbose = FALSE) {
-
   hybrids.bedpe.dt <- find_hybrid_overlaps(hybrids.dt, verbose = verbose)
 
-  if(nrow(hybrids.bedpe.dt) == 0) return(hybrids.dt[, cluster := as.character(NA)])
+  if (nrow(hybrids.bedpe.dt) == 0) {
+    return(hybrids.dt[, cluster := as.character(NA)])
+  }
 
   sel.bedpe.dt <- hybrids.bedpe.dt[L_p > percent_overlap & R_p > percent_overlap]
 
@@ -86,26 +96,27 @@ cluster_hybrids <- function(hybrids.dt, percent_overlap = 0.75, verbose = FALSE)
   igraph::E(g)$weight <- sel.bedpe.dt$mean_p # weight by percent overlap
 
   c <- igraph::components(g)
-  if(verbose) message(c$no, " clusters")
+  if (verbose) message(c$no, " clusters")
 
-  clusters.dt <- data.table(name = names(c$membership),
-                            cluster = c$membership)
+  clusters.dt <- data.table(
+    name = names(c$membership),
+    cluster = c$membership
+  )
   setorder(clusters.dt, cluster)
 
   # Merge back
-  if(nrow(clusters.dt) == 0) clusters.dt[, name := character()] # In case there are no clusters
+  if (nrow(clusters.dt) == 0) clusters.dt[, name := character()] # In case there are no clusters
   setkey(clusters.dt, name)
-  if(nrow(clusters.dt) != 0) stopifnot(any(!duplicated(clusters.dt$name))) # Make sure no hybrid is in more than one cluster, but only if there are clusters
+  if (nrow(clusters.dt) != 0) stopifnot(any(!duplicated(clusters.dt$name))) # Make sure no hybrid is in more than one cluster, but only if there are clusters
   clusters.dt[, cluster := paste0("C", cluster)]
 
   # Merge back
   setkey(hybrids.dt, name)
-  if("cluster" %in% names(hybrids.dt)) hybrids.dt[, cluster := NULL] # if clusters already assigned, remove them
+  if ("cluster" %in% names(hybrids.dt)) hybrids.dt[, cluster := NULL] # if clusters already assigned, remove them
   hybrids.clustered.dt <- merge(hybrids.dt, clusters.dt, by = "name", all.x = TRUE)
   hybrids.clustered.dt[is.na(cluster), cluster := "."]
 
   return(hybrids.clustered.dt)
-
 }
 
 # #' Title
@@ -140,34 +151,42 @@ cluster_hybrids <- function(hybrids.dt, percent_overlap = 0.75, verbose = FALSE)
 
 # }
 
-#' Title
+#' Collapse clusters
 #'
-#' @param hybrids.dt
+#' Collapse overlapping clusters of hybrids into a representative duplex
 #'
-#' @return
+#' @param hybrids.dt Hybrid data.table
+#' @return Clusters data.table
 #' @import data.table
 #' @export
 
 collapse_clusters <- function(hybrids.dt) {
+  if(!"clusters" %in% names(hybrids.dt)) stop("No clusters in hybrids.dt")
 
   clusters.dt <- hybrids.dt[!is.na(cluster) & !is.infinite(cluster)][cluster != ""][cluster != "."]
-  clusters.dt[, `:=` (L_cluster_start = floor(median(L_start)),
-                      L_cluster_end = ceiling(median(L_end)),
-                      R_cluster_start = floor(median(R_start)),
-                      R_cluster_end = ceiling(median(R_end))),
-              by = .(L_seqnames, R_seqnames, cluster)]
+  clusters.dt[, `:=`(
+    L_cluster_start = floor(median(L_start)),
+    L_cluster_end = ceiling(median(L_end)),
+    R_cluster_start = floor(median(R_start)),
+    R_cluster_end = ceiling(median(R_end))
+  ),
+  by = .(L_seqnames, R_seqnames, cluster)
+  ]
 
   clusters.dt[, count := .N, by = .(L_seqnames, R_seqnames, cluster)]
-  clusters.dt <- unique(clusters.dt[, .(L_seqnames, L_cluster_start, L_cluster_end, L_strand,
-                                        R_seqnames, R_cluster_start, R_cluster_end, R_strand,
-                                        cluster, count)])
+  clusters.dt <- unique(clusters.dt[, .(
+    L_seqnames, L_cluster_start, L_cluster_end, L_strand,
+    R_seqnames, R_cluster_start, R_cluster_end, R_strand,
+    cluster, count
+  )])
 
-  setnames(clusters.dt,
-           c("L_cluster_start", "L_cluster_end", "R_cluster_start", "R_cluster_end"),
-           c("L_start", "L_end", "R_start", "R_end"))
+  setnames(
+    clusters.dt,
+    c("L_cluster_start", "L_cluster_end", "R_cluster_start", "R_cluster_end"),
+    c("L_start", "L_end", "R_start", "R_end")
+  )
 
   clusters.dt[, name := paste0(cluster, "-", L_seqnames, "-", R_seqnames)]
 
   return(clusters.dt)
-
 }
--- R/contact_maps.R
+++ R/contact_maps.R
@@ -1,61 +1,52 @@
+# ==========
+# Functions for contact maps
+# ==========
 
-
-#' Title
+#' Get contact map
 #'
-#' @param hybrid.dt
-#' @param genome.size
-#' @param verbose
+#' Gets contact map matrix from hybrids.dt (previously subset to given gene/region)
 #'
-#' @return
+#' @param hybrid.dt Hybrids data.table
+#' @param genome.size Size of genome/gene/region
+#' @param verbose Monitor progress
+#' @return Contact map matrix
 #' @export
-#'
-#' @examples
 
 get_contact_map <- function(hybrid.dt, genome.size, verbose = TRUE) {
-
   hybrid.dt <- primavera::reorient_hybrids(hybrid.dt)
 
   mat <- matrix(data = 0, nrow = genome.size, ncol = genome.size)
 
-  for(i in 1:nrow(hybrid.dt)) {
-
-    if(verbose) if(i%%1e5 == 0) message(i)
-    mat[hybrid.dt[i]$L_start:hybrid.dt[i]$L_end,
-        hybrid.dt[i]$R_start:hybrid.dt[i]$R_end] <- mat[hybrid.dt[i]$L_start:hybrid.dt[i]$L_end,
-                                                        hybrid.dt[i]$R_start:hybrid.dt[i]$R_end] + 1
-
+  for (i in 1:nrow(hybrid.dt)) {
+    if (verbose) if (i %% 1e5 == 0) message(i)
+    mat[
+      hybrid.dt[i]$L_start:hybrid.dt[i]$L_end,
+      hybrid.dt[i]$R_start:hybrid.dt[i]$R_end
+    ] <- mat[
+      hybrid.dt[i]$L_start:hybrid.dt[i]$L_end,
+      hybrid.dt[i]$R_start:hybrid.dt[i]$R_end
+    ] + 1
   }
 
   return(mat)
-
 }
 
-
-#' Title
+#' Bin contact map matrix
 #'
-#' @param mat
-#' @param bin.size
-#'
-#' @return
+#' @param mat Contact map matrix
+#' @param bin.size Size of bin
+#' @return Binnecd contact map matrix
 #' @export
-#'
-#' @examples
 
 bin_matrix <- function(mat, bin.size) {
-
-  mat.size <- nrow(mat)/bin.size
+  mat.size <- nrow(mat) / bin.size
   bin.mat <- matrix(data = 0, nrow = mat.size, ncol = mat.size)
 
-  for(i in seq_len(mat.size)) {
-
-    for(j in seq_len(mat.size)) {
-
-      bin.mat[i, j] <- sum(mat[((i*bin.size) - bin.size + 1):(i*bin.size), ((j*bin.size) - bin.size + 1):(j*bin.size)])
-
+  for (i in seq_len(mat.size)) {
+    for (j in seq_len(mat.size)) {
+      bin.mat[i, j] <- sum(mat[((i * bin.size) - bin.size + 1):(i * bin.size), ((j * bin.size) - bin.size + 1):(j * bin.size)])
     }
-
   }
 
   return(bin.mat)
-
 }
