2c1730ae55e800124d499149a549efdd5b1d7435
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-04-14 21:58:24 +0100

    Moved load_blast8 to new identify_hybrids.R

--- R/identify_hybrids.R
+++ R/identify_hybrids.R
@@ -1,3 +1,36 @@
+# ==========
+# Functions to identify hybrids from Blat output
+# ==========
+
+#' Title
+#'
+#' @param blast8
+#'
+#' @return
+#' @export
+#' @import data.table
+#'
+#' @examples
+
+load_blast8 <- function(blast8) {
+
+  dt <- data.table::fread(input = blast8,
+                          col.names = c("query",
+                                        "subject",
+                                        "identity",
+                                        "alignment_length",
+                                        "mismatches",
+                                        "gap_openings",
+                                        "q_start",
+                                        "q_end",
+                                        "s_start",
+                                        "s_end",
+                                        "evalue",
+                                        "bit_score"))
+  setkey(dt, query)
+  return(dt)
+
+}
 
 #' Title
 #'
