5394a9ea2b6430936807ca87d7eabb8eec9a794d
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-05-08 13:42:16 +0100

    Added utils

---
+++ R/utils.R
@@ -0,0 +1,25 @@
+
+
+#' Get sequence from NCBI
+#'
+#' @param accession NCBI accession
+#'
+#' @return DNAStringSet of sequence
+#' @export
+#'
+#' @examples
+
+get_ncbi_sequence <- function(accession) {
+
+  fa <- rentrez::entrez_fetch(db="nucleotide", id=accession, rettype="fasta")
+  lines <- stringr::str_split(fa, "\n")[[1]]
+  id <- gsub(">", "", lines[1])
+  seq <- paste0(lines[-1], collapse = "")
+
+  # Convert to DNAStringSet
+  dss <- Biostrings::DNAStringSet(seq)
+  names(dss) <- id
+
+  return(dss)
+
+}
